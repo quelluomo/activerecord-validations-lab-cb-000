d21cb8b96398ecc8f073b4dc74b9847e2be5e9ed
Matteo Muscas <thatman.j@gmail.com>
2019-05-30 08:27:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ActiveRecord::Base
 validates :title, uniqueness: true
 validates :content, length: { minimum: 250 }
 validates :summary, length: { minimum: 250 }
-validates :category, inclusion: { in: %w(Fiction Non-Fiction),
-    message: "%{value} is not a valid category" }
+validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
