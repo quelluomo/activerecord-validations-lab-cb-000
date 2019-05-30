1fe8595b3f06da49d2d21d80f06882fa24903827
Matteo Muscas <thatman.j@gmail.com>
2019-05-30 08:59:09 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,10 @@ validate :clickbait
 
   def clickbait
     if self.title == nil &&
-       !title.include?("Won't Believe") ||
-       !title.include?("Secret") ||
-       !title.include?("Guess") ||
-       !title.include?("Top [number]")
+       !self.title.include?("Won't Believe") ||
+       !self.title.include?("Secret") ||
+       !self.title.include?("Guess") ||
+       !self.title.include?("Top [number]")
     errors.add(:title, "Not a clickbait-y title")
     end
   end
