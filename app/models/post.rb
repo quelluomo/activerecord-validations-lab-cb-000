599e1c3d652123095be86afea38ecf4dfb511ccb
Matteo Muscas <thatman.j@gmail.com>
2019-05-30 08:46:03 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,10 @@ validates :summary, length: { maximum: 250 }
 validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 validate :clickbait
 
-def clickbait
-  unless self.title.include?(
-    "Won't Believe" || "Secret" || "Guess" || "Top"
-    )
-  error.add(:clickbaity, "Not a clickbait-y title")
+  def clickbait
+    unless self.title.include?(
+      "Won't Believe" || "Secret" || "Guess" || "Top"
+      )
+    error.add(:clickbaity, "Not a clickbait-y title")
+  end
 end
